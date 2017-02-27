8afd03d45fb8bf356bb48478c1d80617acf3af60
urban-martin <urban.martin@colorado.edu>
2017-02-26 19:43:18 -0700

    comitting slightly modified output example based on Processing_TriggerText_1DTW

--- extractor/ubit-extractor.py
+++ extractor/ubit-extractor.py
@@ -1,13 +1,13 @@
 import serial
 import OSC
+import math
 
 
 # monitor_serial() monitors the specified serial port for a line of
 # data and returns it. Be sure the serial device uses newlines.
 def monitor_serial(serial_path, rate=115200):
-    s = serial.Serial(port=serial_path, baudrate=rate)
-    test_readline = s.readline()
-    return test_readline
+    s = serial.Serial(port=serial_path, baudrate=rate, timeout=None)
+    return s
 
 
 # initialize_osc() returns a OSC client object
@@ -16,6 +16,12 @@ def initialize_osc(dest_ip, dest_port):
     dest = OSC.OSCClient()
     dest.connect(destination)
     return dest
+
+# # initialize_osc_listener() returns a OSC server object
+# def initialize_osc_listener(recv_ip, recv_port):
+#     receive_address = recv_ip, recv_port
+#     server = OSC.OSCServer(receive_address)
+
 
 
 def main():
@@ -26,18 +32,46 @@ def main():
     dest_ip = '127.0.0.1'  # wekinator ip
     dest_port = 6448  # wekinator port
     dest_addr = '/wek/inputs'
+    keys = ['pulse', 'acc_roll', 'temp', 'pos_x', 'poz_y', 'pos_z']
 
+    # pulses_buffer =[]
+    # number_abv_th = 0
+    # threshold = 0
 
     wekinator = initialize_osc(dest_ip, dest_port)
+    serial_thing = monitor_serial(serial_path, serial_rate)
     while True:
-        incoming_data = monitor_serial(serial_path, serial_rate)
+
+        incoming_data = serial_thing.readline()
         osc_msg = OSC.OSCMessage()
         osc_msg.setAddress(dest_addr)
-        data_temp, data_pitch, data_roll = incoming_data.split(',')
-        osc_msg.append(float(data_temp))
-        osc_msg.append(float(data_pitch))
-        osc_msg.append(float(data_roll))
+        values = []
+        values += [0.0 + (int(value) if (value != '' and value != '-') else 0) for value in incoming_data.rstrip().split(",")]
+        if len(values) < 6:
+            #print incoming_data
+            values += [0]
+        #
+        # print threshold
+        # if len(pulses_buffer) < 60:
+        #     pulses_buffer += [values[0]]
+        #     values += [0]
+        # else:
+        #     threshold = max(pulses_buffer) * 0.9
+        #     pulses_buffer.pop(0)
+        #     pulses_buffer += [values[0]]
+        #     number_abv_th = len([a for a in pulses_buffer if a > threshold])
+        #     values += [number_abv_th]
+
+        for ii in range(len(values)):
+            print values
+            #print "Feature" + str(ii) + ": " + str(values[ii])
+            osc_msg.append(float(values[ii]))
         wekinator.send(osc_msg)
+
+        #wekinator.send(osc_msg)
+            # for ii in range(len(values)):
+            #   print "Feature" + str(ii) + ": " + value
+            #time.sleep(0.5)
 
 
 
