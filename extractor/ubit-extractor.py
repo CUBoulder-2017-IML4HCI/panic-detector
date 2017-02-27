2b7b7413bc4f74abab062eb9cac69a328b6f2b85
urban-martin <urban.martin@colorado.edu>
2017-02-26 20:13:06 -0700

    clean up comments, etc

--- extractor/ubit-extractor.py
+++ extractor/ubit-extractor.py
@@ -1,6 +1,5 @@
 import serial
 import OSC
-import math
 
 
 # monitor_serial() monitors the specified serial port for a line of
@@ -17,12 +16,6 @@ def initialize_osc(dest_ip, dest_port):
     dest.connect(destination)
     return dest
 
-# # initialize_osc_listener() returns a OSC server object
-# def initialize_osc_listener(recv_ip, recv_port):
-#     receive_address = recv_ip, recv_port
-#     server = OSC.OSCServer(receive_address)
-
-
 
 def main():
 
@@ -34,10 +27,6 @@ def main():
     dest_addr = '/wek/inputs'
     keys = ['pulse', 'acc_roll', 'temp', 'pos_x', 'poz_y', 'pos_z']
 
-    # pulses_buffer =[]
-    # number_abv_th = 0
-    # threshold = 0
-
     wekinator = initialize_osc(dest_ip, dest_port)
     serial_thing = monitor_serial(serial_path, serial_rate)
     while True:
@@ -48,32 +37,14 @@ def main():
         values = []
         values += [0.0 + (int(value) if (value != '' and value != '-') else 0) for value in incoming_data.rstrip().split(",")]
         if len(values) < 6:
-            #print incoming_data
             values += [0]
-        #
-        # print threshold
-        # if len(pulses_buffer) < 60:
-        #     pulses_buffer += [values[0]]
-        #     values += [0]
-        # else:
-        #     threshold = max(pulses_buffer) * 0.9
-        #     pulses_buffer.pop(0)
-        #     pulses_buffer += [values[0]]
-        #     number_abv_th = len([a for a in pulses_buffer if a > threshold])
-        #     values += [number_abv_th]
+
 
         for ii in range(len(values)):
-            print values
-            #print "Feature" + str(ii) + ": " + str(values[ii])
             osc_msg.append(float(values[ii]))
         wekinator.send(osc_msg)
 
-        #wekinator.send(osc_msg)
-            # for ii in range(len(values)):
-            #   print "Feature" + str(ii) + ": " + value
-            #time.sleep(0.5)
 
-
-
+# Call main()
 if __name__ == '__main__':
    main()
